469d0edfabf8e65645435b7429b24687f9405157
Tarun Miglani <tarunmiglani399@gmail.com>
2022-07-05 18:11:42 +0530

    Initialize

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const app = require("./app");
 const logger = require("./config/logger");
+const express = require("express");
 
 let server;
 const options = {
