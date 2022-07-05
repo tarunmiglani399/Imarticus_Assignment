5fd027b53dc451ad5ab6a206b318085db7684b7a
Tarun Miglani <tarunmiglani399@gmail.com>
2022-07-05 18:38:11 +0530

    Initialize

--- src/index.js
+++ src/index.js
@@ -15,8 +15,9 @@ if (
   process.env.NODE_ENV === "staging"
 ) {
   app.use(express.static("frontend/build"));
+  const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
